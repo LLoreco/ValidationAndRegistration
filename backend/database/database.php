ba13552ae9b546882a352018d1075a115bb564d6
Kirill Safin <safinkirill2003@mail.ru>
2025-06-13 02:20:19 +0500

    connection test and fixes

---
+++ backend/database/database.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'PHPAuthAndRegister';
+    private $user = 'postgres';
+    private $password = 'Abcdefg111';
+    private $connection;
+
+    public function connect() {
+        $this->connection = null;
+        try {
+            $this->connection = new PDO(
+                "pgsql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->user,
+                $this->password
+            );
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->connection;
+        } catch (PDOException $e) {
+            echo "Database connection error: " . $e->getMessage();
+            return null;
+        }
+    }
+}
+?>
